5f16f4fb1d7a7989fc50bc96f4b0ee2254d1616e
Piotr Kilczuk <piotr@tymaszweb.pl>
2022-04-09 21:48:54 +0200

    Choose quiz difficulty level
    Signed-off-by: Piotr Kilczuk <piotr@tymaszweb.pl>

--- quizzes/views.py
+++ quizzes/views.py
@@ -1,4 +1,5 @@
-from flask import render_template
+from flask import render_template, request
+from flask_user import login_required
 
 from quizzes.app import app
 
@@ -6,3 +7,16 @@ from quizzes.app import app
 @app.route("/")
 def index():
     return render_template("index.html")
+
+
+@app.route("/quiz")
+@login_required
+def choose_quiz_difficulty():
+    return render_template("choose_quiz_difficulty.html")
+
+
+@app.route("/quiz/:difficulty")
+@login_required
+def take_quiz():
+
+    raise NotImplementedError(request.method)
